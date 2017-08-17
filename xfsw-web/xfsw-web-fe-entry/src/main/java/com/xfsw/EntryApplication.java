9c1d1cba8b7070494d2fc7f9b1892dea2419329a
boy1064910 <277192937@qq.com>
2017-08-18 02:29:36 +0800

    0.0.1

--- xfsw-web/xfsw-web-fe-entry/src/main/java/com/xfsw/EntryApplication.java
+++ xfsw-web/xfsw-web-fe-entry/src/main/java/com/xfsw/EntryApplication.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package com.xfsw.bootstrap;
+package com.xfsw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
